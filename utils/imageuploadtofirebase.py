a9667b6146c256e6bc65fa4f8ac5add9f4419fa9
Obi Ebuka Obi <davidobi023@gmail.com>
2020-01-14 15:16:15 +0100

    Update imageuploadtofirebase.py

--- utils/imageuploadtofirebase.py
+++ utils/imageuploadtofirebase.py
@@ -13,14 +13,4 @@ firebase = pyrebase.initialize_app(config)
 
 storage = firebase.storage()
 
-#https://banking-4ad45.firebaseio.com/
-#gs://banking-4ad45.appspot.com
 
-
-
-config = {
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com"
-}
